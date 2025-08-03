08e3982eccfc6fb9825495d7cd68ba5e5c6ab3d3
bqbs <codemonkeyjc@gmail.com>
2025-08-04 04:00:54 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -15,7 +15,7 @@ const getBasePath = () => {
 };
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
+  siteTitle: '跑步日记',
   siteUrl: 'https://bqbs.github.io/running_page/',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
